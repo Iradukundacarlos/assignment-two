b83b065ed5af0a37832e75763b56359bd293ef5d
HP <carlosiradukunda0@gmail.com>
2024-06-25 16:21:55 +0200

    yolla

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,380 @@
-import 'package:flutter/material.dart';
-import 'package:calculatorapp/button.dart';
+                                                                                             import 'package:flutter/material.dart';
 
 void main() {
-  runApp(calculatorapp());
+  runApp(MyApp());
 }
 
-class calculatorapp extends StatefulWidget {
-  const calculatorapp({super.key});
-
-  @override
-  State<calculatorapp> createState() => _calculatorappState();
-}
-
-class _calculatorappState extends State<calculatorapp> {
-  final List<String> buttons = [
-    'C',
-    'DEL',
-    '%',
-    '/',
-    '9',
-    '8',
-    '7',
-    'x',
-    '6',
-    '5',
-    '4',
-    '-',
-    '3',
-    '2',
-    '1',
-    '+',
-    '0',
-    '.',
-    'ANS',
-    '=',
-  ];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Expanded(
-              child: Container(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _screens = [
+    SignInScreen(),
+    SignUpScreen(),
+    CalculatorScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Sign In',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.app_registration),
+            label: 'Sign Up',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculator',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Sign In'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _selectedIndex = 0;
+                });
+              },
+            ),
+            ListTile(
+              title: Text('Sign Up'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _selectedIndex = 1;
+                });
+              },
+            ),
+            ListTile(
+              title: Text('Calculator'),
+              onTap: () {
+                Navigator.pop(context);
+                setState(() {
+                  _selectedIndex = 2;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignInScreen extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Welcome Back!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            controller: _emailController,
+            decoration: InputDecoration(labelText: 'Email'),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(labelText: 'Password'),
+            obscureText: true,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Implement sign-in logic here
+            },
+            child: Text('Sign In'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SignUpScreen extends StatelessWidget {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlueAccent,
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Create an Account',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              labelText: 'Name',
+              labelStyle: TextStyle(color: Colors.white),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            keyboardType: TextInputType.name,
+            style: TextStyle(color: Colors.white),
+          ),
+          TextField(
+            controller: _phoneController,
+            decoration: InputDecoration(
+              labelText: 'Phone Number',
+              labelStyle: TextStyle(color: Colors.white),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            keyboardType: TextInputType.phone,
+            style: TextStyle(color: Colors.white),
+          ),
+          TextField(
+            controller: _emailController,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              labelStyle: TextStyle(color: Colors.white),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white),
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              labelText: 'Password',
+              labelStyle: TextStyle(color: Colors.white),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.white),
+              ),
+            ),
+            obscureText: true,
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Implement sign-up logic here
+            },
+            child: Text('Sign Up'),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white, backgroundColor: Colors.blueAccent, // foreground
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CalculatorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Calculator',
+        home: Calculation(),
+        theme: ThemeData.light(),
+    );
+  }
+}
+
+class Calculation extends StatefulWidget{
+  @override
+  _CalculationState createState() => _CalculationState();
+}
+
+class _CalculationState extends State<Calculation> {
+  List<dynamic> inputList = [0];
+  String output = '0';
+
+  void _handleClear() {
+    setState(() {
+      inputList = [0];
+      output = '0';
+    });
+  }
+
+  void _handlePress(String input) {
+    setState(() {
+      if (_isOperator(input)) {
+        if (inputList.last is int) {
+          inputList.add(input);
+          output += input;
+        }
+      } else if (input == '=') {
+        while (inputList.length > 2) {
+          int firstNumber = inputList.removeAt(0) as int;
+          String operator = inputList.removeAt(0);
+          int secondNumber = inputList.removeAt(0) as int;
+          int partialResult = 0;
+
+          if (operator == '+') {
+            partialResult = firstNumber + secondNumber;
+          } else if (operator == '-') {
+            partialResult = firstNumber - secondNumber;
+          } else if (operator == '*') {
+            partialResult = firstNumber * secondNumber;
+          } else if (operator == '/') {
+            partialResult = firstNumber ~/ secondNumber;
+            // Protect against division by zero
+            if(secondNumber == 0) {
+              partialResult = firstNumber;
+            }
+          }
+
+          inputList.insert(0, partialResult);
+        }
+
+        output = '${inputList[0]}';
+      } else {
+        int? inputNumber = int.tryParse(input);
+        if (inputNumber != null) {
+          if (inputList.last is int && !_isOperator(output[output.length - 1])) {
+            int lastNumber = (inputList.last as int);
+            lastNumber = lastNumber * 10 + inputNumber;
+            inputList.last = lastNumber;
+
+            output = output.substring(0, output.length - 1) + lastNumber.toString();
+          } else {
+            inputList.add(inputNumber);
+            output += input;
+          }
+        }
+      }
+    });
+  }
+
+  bool _isOperator(String input) {
+    if (input == "+" || input == "-" || input == "*" || input == "/") {
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Calculator")),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: TextField(
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.right,
+                decoration: InputDecoration(border: InputBorder.none,),
+                controller: TextEditingController()..text = output,
+                readOnly: true,
+              ),
             ),
             Expanded(
-              flex: 2,
-              child: Container(
-                child: GridView.builder(
-                    itemCount: buttons.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      childAspectRatio: 1.2,
-                      crossAxisSpacing: 8.0,
-                      mainAxisSpacing: 8.0,
+              child: GridView.count(
+                crossAxisCount: 4,
+                children: <Widget>[
+                  for(var i = 0; i <= 9; i++)
+                    TextButton(
+                      child: Text("$i", style: TextStyle(fontSize: 25)),
+                      onPressed: () => _handlePress("$i"),
                     ),
-                    itemBuilder: (BuildContext context, index) {
-                      return Button(
-                        buttonTxt: buttons[index],
-                        Color: Colors.blue,
-                        textColor: Colors.black,
-                      );
-                    }),
+                  TextButton(
+                    child: Text("C", style: TextStyle(fontSize: 25)),
+                    onPressed: _handleClear,
+                  ),
+                  TextButton(
+                    child: Text("+", style: TextStyle(fontSize: 25)),
+                    onPressed: () => _handlePress("+"),
+                  ),
+                  TextButton(
+                    child: Text("-", style: TextStyle(fontSize: 25)),
+                    onPressed: () => _handlePress("-"),
+                  ),
+                  TextButton(
+                    child: Text("*", style: TextStyle(fontSize: 25)),
+                    onPressed: () => _handlePress("*"),
+                  ),
+                  TextButton(
+                    child: Text("/", style: TextStyle(fontSize: 25)),
+                    onPressed: () => _handlePress("/"),
+                  ),
+                  TextButton(
+                    child: Text("=", style: TextStyle(fontSize: 25)),
+                    onPressed: () => _handlePress("="),
+                  ),
+                ],
               ),
             )
           ],
